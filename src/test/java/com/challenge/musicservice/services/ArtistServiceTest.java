d10df90188a26f006943acb198ffb6fa8e5902ab
Agustin Espana <agustinespania@gmail.com>
2022-08-12 12:26:49 +0200

    added testing

---
+++ src/test/java/com/challenge/musicservice/services/ArtistServiceTest.java
@@ -0,0 +1,50 @@
+package com.challenge.musicservice.services;
+
+import com.challenge.musicservice.pojos.MBArtist;
+import com.challenge.musicservice.pojos.Relation;
+import com.challenge.musicservice.pojos.RelationUrl;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.mockito.InjectMocks;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class ArtistServiceTest {
+
+    @InjectMocks
+    ArtistService artistService;
+
+    MBArtist artist = new MBArtist();
+
+    @BeforeAll
+    public void setUp() {
+        artist.setName("Michael Jackson");
+        artist.setGender("Male");
+        artist.setCountry("US");
+        artist.setDisambiguation("“King of Pop”");
+
+        Relation relation = new Relation();
+        relation.setType("wikidata");
+        RelationUrl relationUrl = new RelationUrl();
+        relationUrl.setResource("https://www.wikidata.org/wiki/Special:EntityData/Q2831");
+        relation.setUrl(relationUrl);
+        List<Relation> list = new ArrayList<>();
+        list.add(relation);
+        artist.setRelations(list);
+    }
+
+    @Test
+    public void shouldReturnWikidataLink() {
+        String expectedUrl = "https://www.wikidata.org/wiki/Special:EntityData/Q2831.json";
+        String testUrl = artistService.getWikidataLink(artist);
+
+        assertEquals(expectedUrl, testUrl);
+    }
+}
